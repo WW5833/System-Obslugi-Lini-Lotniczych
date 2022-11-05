6fc08e40e6a82969158236f96046c5d8529b63d8
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-11-05 20:43:44 +0100

    Dodaj pliki projektów.

---
+++ System-Obslugi-Lini-Lotniczych/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System_Obslugi_Lini_Lotniczych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
